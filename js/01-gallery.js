0957fc2f2d9137478e05ec17aaac30a50d17b4a3
Andrii Chychkan <mywork1728@gmail>
2023-06-06 22:27:55 +0300

    finished first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -13,6 +13,7 @@ const makeElem = ({ preview, original, description }) => {
           src="${preview}"
           data-source="${original}"
           alt="${description}"
+          width="340px"
         />
       </a>
     </li>`;
@@ -21,3 +22,25 @@ const makeElem = ({ preview, original, description }) => {
 const markUp = galleryItems.map(el => makeElem(el));
 
 mainList.insertAdjacentHTML('afterbegin', markUp.join(' '));
+
+mainList.addEventListener('click', openFullImg);
+
+function openFullImg(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" />`
+  );
+
+  instance.show();
+
+  document.addEventListener('keydown', (event, openFullImg) => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
