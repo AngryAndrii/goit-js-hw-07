1659b570edcc7a7ebf49ccb87ed6bdff36d6539f
Andrii Chychkan <mywork1728@gmail>
2023-06-07 22:24:04 +0300

    finished all tasks

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const mainList = document.querySelector('.gallery');
+
+const makeElem = ({ preview, original, description }) => {
+  return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+};
+
+const markUp = galleryItems.map(el => makeElem(el));
+
+mainList.insertAdjacentHTML('afterbegin', markUp.join(' '));
+
+mainList.addEventListener('click', openFullImg);
+
+function openFullImg(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
